f4b754744855d46c3c5026ca5bac9e76afd823eb
megib16 <megibida@Megis-MacBook-Air.local>
2026-06-23 15:59:28 +0200

    Summarizer done

--- backend/summarizer.py
+++ backend/summarizer.py
@@ -16,14 +16,30 @@ def format_result_for_prompt(results, race_info):
         time = driver["Time"]
         lines.append(f"P{position}: {name} ({team}) - {time}")
     
-    return "\n".join(lines)  
+    return "\n".join(lines) 
 
+
+def format_pitstops_for_prompt(race_info, pit_stops): 
+    lines = [] 
+    lines.append(f" {race_info['name']} Pit stop information") 
+    lines.append("") 
+    sorted_pitstops = sorted(pit_stops, key = lambda x: x["LapNumber"])
+    for lap in sorted_pitstops: 
+            name = lap["Driver"]
+            lap_number = lap["LapNumber"]  
+            compound = lap["Compound"] 
+            lines.append(f"{name} pitted on lap {lap_number} ({compound})") 
+    return "\n".join(lines)
+
+
+#Driver, LapNumber, Compound. Build a line for each one like: Russell pitted on lap 32 (Medium)  
 if __name__ == "__main__":
     session = fetcher.load_session(2026, "Australia")
     results = fetcher.get_race_results(session)
     race_info = fetcher.get_race_info(session)
     pit_stops = fetcher.get_pitstop_data(session)
 
-    print(format_result_for_prompt(results, race_info))
+    print(format_result_for_prompt(results, race_info)) 
+    print(format_pitstops_for_prompt(race_info, pit_stops))
 
 
