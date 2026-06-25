1c7a8ae8ce27d32e5f0acbc7150d9a0bff09a8da
megib16 <megibida@Megis-MacBook-Air.local>
2026-06-25 15:56:06 +0200

    Models done

---
+++ backend/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker 
+from models import Base 
+
+engine = create_engine("sqlite:///f1_races.db") 
+SessionLocal = sessionmaker(bind = engine) 
+Base.metadata.create_all(engine)
--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,30 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey 
-from sqlalchemy.orm import DeclarativeBase 
+from sqlalchemy.orm import declarative_base 
 
-Base = declarative_base()
+Base = declarative_base() 
+
+class Race(Base): 
+        __tablename__ = "races"
+        race_id = Column(Integer, primary_key = True) 
+        name = Column(String) 
+        location = Column(String) 
+        date = Column(String) 
+        round = Column(Integer) 
+        total_laps = Column(Integer) 
+        summary = Column(String) 
+
+class DriverResult(Base): 
+        __tablename__ = "results"
+        driver_id = Column(Integer, primary_key = True) 
+        race_id = Column(Integer, ForeignKey("races.race_id")) 
+        position = Column(Integer) 
+        full_name = Column(String) 
+        team = Column(String) 
+        time = Column(String)         
+        
+
+
+
 #Base — one line: Base = declarative_base() — this is what all your table classes inherit from
 
 #Race class — columns: id (Integer, primary key), name, location, date (all String), round, total_laps (both Integer), summary (String — for the AI text later)
